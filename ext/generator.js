cbb9c408a510052e8e981b7fde63818d17bb4ed0
Fakhrul Rijal <5477182+frijal@users.noreply.github.com>
2025-11-15 13:24:45 +0800

    Update generator.js

--- ext/generator.js
+++ ext/generator.js
@@ -136,8 +136,8 @@ export const Sitemap = {
 `  <url>
     <loc>${Util.escapeXml(`${CONFIG.baseUrl}/artikel/${file}`)}</loc>
     <lastmod>${Util.escapeXml(lastmod)}</lastmod>
-    <priority>${CONFIG.xmlPriority}</priority>
     <changefreq>${CONFIG.xmlChangeFreq}</changefreq>
+    <priority>${CONFIG.xmlPriority}</priority>
     <image:image>
       <image:loc>${Util.escapeXml(image)}</image:loc>
     </image:image>
@@ -148,7 +148,8 @@ export const Sitemap = {
     return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
-${entries.join("\n")} 
+${entries.join("
+")} 
 </urlset>`;
   }
 };
