e6709a79d10c75e564e5cdbf0f5e588d3bb5de2f
frijal <5477182+frijal@users.noreply.github.com>
2025-12-06 20:42:50 +0800

    coba pesbuk

--- ext/pesbuk.js
+++ ext/pesbuk.js
@@ -1,66 +1,68 @@
 (function () {
   const d = document;
 
-  // --- CREATE fb-root AUTOMATICALLY ---
+  /* --------------------------------------------------
+     Target widget container
+  -------------------------------------------------- */
+  const box = d.getElementById("comment");
+  if (!box) return;
+
+  /* --------------------------------------------------
+     Create fb-root automatically (only once)
+  -------------------------------------------------- */
   if (!d.getElementById("fb-root")) {
     const fbroot = d.createElement("div");
     fbroot.id = "fb-root";
     d.body.prepend(fbroot);
   }
 
-  // --- AUTO INSERT FB WIDGETS ---
-  const likeBox = d.getElementById("like");
-  const commentBox = d.getElementById("comment");
+  /* --------------------------------------------------
+     Auto-detect URL from data-href or location.href
+  -------------------------------------------------- */
+  const url = box.dataset.href || location.href;
 
-  if (likeBox) {
-    likeBox.innerHTML = `
-      <div class="fb-like"
-        data-href="${location.href}"
-        data-layout="standard"
-        data-share="true">
-      </div>
-    `;
-  }
+  /* --------------------------------------------------
+     Fill LIKE + COMMENT widgets automatically
+  -------------------------------------------------- */
+  box.innerHTML = `
+    <!-- LIKE BUTTON -->
+    <div class="fb-like"
+         data-href="${url}"
+         data-layout="standard"
+         data-share="true"
+         style="margin-bottom:16px;">
+    </div>
 
-  if (commentBox) {
-    commentBox.innerHTML = `
-      <div class="fb-comments"
-        data-href="${location.href}"
-        data-width="100%"
-        data-numposts="5">
-      </div>
-    `;
-  }
+    <!-- COMMENT BOX -->
+    <div class="fb-comments"
+         data-href="${url}"
+         data-width="100%"
+         data-numposts="5">
+    </div>
+  `;
 
-  // --- FACEBOOK SDK LOADER ---
-  function loadSDK(cb) {
-    if (window.FB && FB.XFBML) return cb();
+  /* --------------------------------------------------
+     Load Facebook SDK (only once)
+  -------------------------------------------------- */
+  function loadSDK() {
+    if (window.FB && FB.XFBML) {
+      FB.XFBML.parse(box);
+      return;
+    }
 
     const s = d.createElement("script");
     s.async = true;
     s.defer = true;
     s.crossOrigin = "anonymous";
-    s.src = "https://connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v24.0&appId=700179713164663";
-    s.onload = cb;
+    s.src =
+      "https://connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v24.0&appId=700179713164663";
+
+    s.onload = () => {
+      if (window.FB && FB.XFBML) FB.XFBML.parse(box);
+    };
+
     d.body.appendChild(s);
   }
 
-  // --- LAZY LOAD (Observer) ---
-  const lazyTarget = likeBox || commentBox;
-  if (!lazyTarget) return;
-
-  const obs = new IntersectionObserver((entries) => {
-    entries.forEach((e) => {
-      if (e.isIntersecting) {
-        loadSDK(() => {
-          if (window.FB && FB.XFBML) {
-            FB.XFBML.parse(); // parses ALL widgets: like + comment
-          }
-        });
-        obs.disconnect();
-      }
-    });
-  }, { rootMargin: "200px" });
-
-  obs.observe(lazyTarget);
+  loadSDK();
 })();
