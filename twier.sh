948c32c90a5548c2eee60ce76c9ec3b3082a4c6d
frijal <5477182+frijal@users.noreply.github.com>
2026-01-06 07:55:30 +0800

    update 01-06

--- twier.sh
+++ twier.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Script untuk mengganti twitter:site content="@frijal" menjadi "@responaja"
-# Hanya pada tag meta twitter:site, tidak mengganti @frijal di tempat lain
-# Berlaku untuk semua file .html secara recursive
+# Script untuk mengganti twitter.com/frijal menjadi twitter.com/responaja
+# Hanya pada URL Twitter/X, di semua file .html secara recursive
+# Tidak mengubah @frijal atau teks lainnya
 
 echo "Mencari file .html di folder ini dan subfolder..."
 
@@ -16,18 +16,33 @@ fi
 echo "Membuat backup file (.html.bak)..."
 find . -type f -name "*.html" -exec cp -p {} {}.bak \;
 
-# Lakukan penggantian hanya pada bagian twitter:site content
-echo "Mengganti twitter:site content=\"@frijal\" menjadi \"@responaja\"..."
+# Lakukan penggantian hanya pada pola twitter.com/frijal
+# Pola ini mencakup variasi umum seperti:
+# https://twitter.com/frijal
+# http://twitter.com/frijal
+# twitter.com/frijal
+# www.twitter.com/frijal
+# dan dengan atau tanpa path tambahan (/status/... dll)
+
+echo "Mengganti twitter.com/frijal → twitter.com/responaja ..."
 find . -type f -name "*.html" -exec sed -i \
-    -e 's/\(twitter:site"[^>]*content="\)[@]frijal\("\)/\1@responaja\2/gI' {} +
+    -e 's|twitter\.com/frijal|twitter.com/responaja|gI' \
+    -e 's|www\.twitter\.com/frijal|www.twitter.com/responaja|gI' \
+    -e 's|https\?://twitter\.com/frijal|https://twitter.com/responaja|gI' \
+    -e 's|https\?://www\.twitter\.com/frijal|https://twitter.com/responaja|gI' {} +
 
 echo "Selesai!"
 echo ""
-echo "Penggantian hanya dilakukan pada tag meta dengan name=\"twitter:site\""
-echo "Username @frijal di tempat lain (misalnya di teks konten) tetap tidak berubah."
+echo "Penggantian hanya dilakukan pada URL Twitter/X yang mengarah ke frijal."
+echo "Contoh yang diubah:"
+echo "   https://twitter.com/frijal      → https://twitter.com/responaja"
+echo "   twitter.com/frijal/status/123   → twitter.com/responaja/status/123"
+echo ""
+echo "Username @frijal di teks atau meta tag tetap tidak berubah."
 echo ""
 echo "Total file .html yang diproses:"
 find . -type f -name "*.html" | wc -l
 echo ""
-echo "Backup tersedia dengan ekstensi .bak. Jika ingin mengembalikan:"
+echo "Backup tersedia dengan ekstensi .bak."
+echo "Untuk mengembalikan perubahan:"
 echo "   find . -name '*.html.bak' -exec sh -c 'mv \"\$0\" \"\${0%.bak}\"' {} \\;"
