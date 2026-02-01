388c40e2afaa35c426be3a943a1901cc5749b37a
frijal <5477182+frijal@users.noreply.github.com>
2026-02-02 02:14:30 +0800

    update markdown

--- ext/markdown.js
+++ ext/markdown.js
@@ -115,7 +115,7 @@
 
   // === 4️⃣ Proses Markdown di halaman ==
   function enhanceMarkdown() {
-    const selector = "p, ol, ul, li, blockquote, td, th, h1, h2, h3, h4, h5, h6, .note, .method-card, .code-block, .note-box, .callout, .warning-box, .item, .warning, .quote, .disclaimer, .quote-box, .danger-box, .alert-box, .markdown, .markdown-body, .meta, .card, .info-box, .tips, .tip, .alert, .intro-alert";
+    const selector = "p, ol, ul, li, blockquote, td, th, h1, h2, h3, h4, h5, h6, .note, .method-card, .code-block, .note-box, .callout, .warning-box, .item, .warning, .quote, .disclaimer, .quote-box, .danger-box, .alert-box, .kuhp-point, .contact, .highlight, .closing, .markdown, .markdown-body, .meta, .card, .info-box, .tool-item, .tips, .tip, .alert, .intro-alert";
 
     document.querySelectorAll(selector).forEach(el => {
       if (el.classList.contains("no-md")) return;
