5b90a8b0956314221240b46a1028833ced4aed9b
Fakhrul Rijal <5477182+frijal@users.noreply.github.com>
2026-03-05 23:08:35 +0000

    fxhdfh dhdsr

--- translate-html.ts
+++ translate-html.ts
@@ -69,7 +69,7 @@ ${html}
     method:"POST",
     headers:{"Content-Type":"application/json"},
     body:JSON.stringify({
-      model:"llama3",
+      model:"mistral",
       prompt,
       stream:false
     })
