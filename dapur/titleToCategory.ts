4d64b42f9cf86ff80680a6ed5e89a57f61fad402
frijal <5477182+frijal@users.noreply.github.com>
2026-07-05 13:44:22 +0800

    07-05

--- dapur/titleToCategory.ts
+++ dapur/titleToCategory.ts
@@ -10,37 +10,37 @@ const categories: Category[] = [
   {
     name: "Sistem Terbuka",
     keywords: [
-      "linux", "ubuntu", "debian", "arch", "fedora", "Clonezilla", "Kubuntu", "Synaptic", "cachyos", "mx-linux", "nixos", "opensuse", "slackware", "zorin", "garuda", "Lubuntu", "blankon", "Mandrake", "mageia", "parrot", "apt", "pacman", "paru", "aur", "dpkg", "yum", "dnf", "OpenMandriva", "flatpak", "snap", "brew", "Perl", "Solaris", "gnome", "kde", "xfce", "hyprland", "wayland", "x11", "compiz", "kernel", "systemd", "grub", "btrfs", "ext4", "chroot", "root", "sudo", "bash", "zsh", "fish", "nano", "vim", "CentOS", "foss", "open source", "oss", "distro", "desktop", "repo", "mirror", "ventoy", "rescuezilla", "rsync", "tar", "shell", "dotfiles", "lts", "glibc"
+      "linux", "ubuntu", "debian", "arch", "Terminal", "fedora", "Clonezilla", "Manjaro","Kubuntu", "Synaptic", "cachyos", "mx-linux", "nixos", "opensuse", "Cinnamon","slackware", "zorin", "garuda", "Lubuntu", "blankon", "Mandrake", "mageia", "parrot", "apt", "pacman", "paru", "aur", "dpkg", "yum", "dnf", "OpenMandriva", "flatpak", "snap", "brew", "Perl", "Solaris", "gnome", "kde", "xfce", "hyprland", "wayland", "x11", "compiz", "kernel", "systemd", "grub", "btrfs", "ext4", "chroot", "root", "sudo", "bash", "zsh", "fish", "nano", "vim", "CentOS", "foss", "open source", "oss", "distro", "desktop", "repo", "mirror", "ventoy", "rescuezilla", "rsync", "tar", "shell", "dotfiles", "lts", "glibc"
     ]
   },
   {
     name: "Olah Media",
     keywords: [
-      "ffmpeg", "handbrake", "excel", "imagemagick", "Visual","Editor", "libreoffice", "ghostscript", "pdftk", "gimp", "inkscape", "video", "audio", "mp4", "mkv", "mp3", "ogg", "webp", "png", "svg", "edit", "convert", "compress", "resize", "crop", "split", "gabung", "Viewer", "watermark", "subtitle", "srt", "transcribe", "ocr", "metadata", "exif", "batch", "rekam", "screencast", "thumbnail", "framerate", "bitrate", "codec", "h264", "h265", "av1"
+      "ffmpeg", "handbrake", "excel", "Foto","Gambar","imagemagick", "Media", "Visual","Editor", "libreoffice", "ghostscript", "pdftk", "gimp", "inkscape", "video", "audio", "mp4", "mkv", "mp3", "ogg", "webp", "png", "svg", "edit", "convert", "compress", "resize", "crop", "split", "gabung", "Viewer", "watermark", "subtitle", "srt", "transcribe", "ocr", "metadata", "exif", "batch", "rekam", "screencast", "thumbnail", "framerate", "bitrate", "codec", "h264", "h265", "av1"
     ]
   },
   {
     name: "Jejak Sejarah",
     keywords: [
-      "nabi", "rasul", "sahabat", "khalifah", "sejarah","Al-Qur'an", "kisah", "islam", "muhammadiyah", "sirah", "madinah", "mekkah", "hijrah", "badar", "uhud", "khandaq", "yarmuk", "hittin", "shalahuddin", "fatih", "muhammad", "ayat", "ibrahim", "iman", "muslim", "musa", "isa", "nuh", "yunus", "perang", "penaklukan", "kekhalifahan", "daulah", "utsmaniyah", "abbasiyah", "andalusia", "baghdad", "cordoba", "jerusalem", "baitul hikmah"
+      "nabi", "rasul", "sahabat", "khalifah", "fitnah", "shalat", "Mahram", "istighfar","Nasab", "Akidah", "Hijab", "Allah", "Risalah","Hijriyah", "Sunnah", "doa", "Ghibah", "surga", "sejarah","Al-Qur'an", "kisah", "islam", "muhammadiyah", "sirah", "madinah", "mekkah", "hijrah", "badar", "uhud", "khandaq", "yarmuk", "hittin", "shalahuddin", "fatih", "muhammad", "ayat", "ibrahim", "iman", "muslim", "musa", "isa", "nuh", "yunus", "perang", "penaklukan", "kekhalifahan", "daulah", "utsmaniyah", "abbasiyah", "andalusia", "baghdad", "cordoba", "jerusalem", "baitul hikmah", "Dakwah"
     ]
   },
   {
     name: "Gaya Hidup",
     keywords: [
-      "kesehatan", "pencernaan", "tidur", "diet", "Sajadah","koperasi", "herbal", "obat", "sakit", "kopi", "minuman", "makanan", "resep", "kurma", "susu", "camilan", "motor", "mobil", "ojol", "touring", "wisata", "traveling", "hotel", "hobi", "gaya hidup", "bahagia", "mental", "stres", "olahraga", "anak", "bahaya", "listrik"
+      "kesehatan", "pencernaan", "tidur", "diet", "lisensi", "Angkringan", "Psikotes","medsos", "Lembur","Konsumen","Pelajar","Sajadah","koperasi", "herbal", "obat", "sakit", "kopi", "minuman", "makanan", "resep", "kurma", "susu", "camilan", "motor", "mobil", "ojol", "touring", "wisata", "traveling", "hotel", "hobi", "gaya hidup", "bahagia", "mental", "stres", "olahraga", "anak", "bahaya", "listrik"
     ]
   },
   {
     name: "Opini Sosial",
     keywords: [
-      "politik", "pemerintah", "korupsi", "demokrasi", "Bangsa","pemilu", "lagu", "presiden", "balikpapan", "mbg", "guru honorer", "pendidikan", "ekonomi", "publik", "negara", "fakta", "indonesia", "umkm", "pajak", "subsidi", "kebijakan", "kritik", "opini", "sosial", "masyarakat", "keadilan", "koruptor", "integritas", "rakyat", "sekolah", "pemimpin", "bisnis"
+      "politik", "pemerintah", "korupsi", "Adab", "Kosakata", "apbn", "oknum", "nasional", "Moral","Psikologis","Pegawai","demokrasi", "Bangsa","pemilu", "mk", "lagu", "presiden", "balikpapan", "mbg", "guru honorer", "pendidikan", "ekonomi", "publik", "negara", "fakta", "indonesia", "umkm", "pajak", "subsidi", "kebijakan", "kritik", "opini", "sosial", "masyarakat", "keadilan", "koruptor", "integritas", "rakyat", "sekolah", "pemimpin", "bisnis"
     ]
   },
   {
     name: "Warta Tekno",
     keywords: [
-      "ai", "chatgpt", "gemini", "claude", "ISO","microsoft", "USB", "internet", "llm", "server", "deep learning", "prompt", "online", "github", "git", "programming", "javascript", "python", "html", "css", "markdown", "workflow", "github actions", "api", "backend", "json", "frontend", "fullstack", "windows", "android", "browser", "firefox", "chrome", "laptop", "hardware", "ssd", "ram", "vram", "zram", "cpu", "gpu", "wifi", "cloudflare", "security", "phishing", "encryption", "privacy", "backup", "ssh", "digital", "kalkulator", "js", "layar", "generator", "modern"
+      "ai", "chatgpt", "gemini", "claude", "HDD", "google", "renovate", "dependabot", "Floorp", "Vivaldi","ISO","microsoft", "USB", "internet", "llm", "server", "deep learning", "prompt", "online", "github", "git", "programming", "javascript", "python", "html", "css", "markdown", "domain", "workflow", "github actions", "api", "backend", "json", "frontend", "fullstack", "windows", "android", "browser", "firefox", "chrome", "laptop", "hardware", "ssd", "ram", "vram", "zram", "cpu", "gpu", "wifi", "cloudflare", "security", "phishing", "encryption", "privacy", "backup", "ssh", "digital", "kalkulator", "js", "layar", "generator", "modern"
     ]
   }
 ];
