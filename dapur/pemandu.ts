8c3326aa28192454ee1b91c8849612b4e546a2d8
frijal <5477182+frijal@users.noreply.github.com>
2026-07-05 11:18:00 +0800

    07-05

--- dapur/pemandu.ts
+++ dapur/pemandu.ts
@@ -309,6 +309,15 @@ ${prevNextHtml}
 </svg>
 </a>
 
+<a href="https://www.linkedin.com/shareArticle?mini=true&url=${encodedLink}&title=${encodedText}&summary=${encodedText}"
+onclick="window.open(this.href,'_blank','noopener,noreferrer,width=600,height=600');return false;"
+title="Bagikan ke LinkedIn" aria-label="Bagikan ke LinkedIn">
+<svg width="20" height="20" viewBox="0 0 122.88 122.31">
+<path fill="currentColor" d="M27.75,0H95.13a27.83,27.83,0,0,1,27.75,27.75V94.57a27.83,27.83,0,0,1-27.75,27.74H27.75A27.83,27.83,0,0,1,0,94.57V27.75A27.83,27.83,0,0,1,27.75,0Z"/>
+<path fill="#fff" d="M49.19,47.41H64.72v8h.22c2.17-3.88,7.45-8,15.34-8,16.39,0,19.42,10.2,19.42,23.47V98.94H83.51V74c0-5.71-.12-13.06-8.42-13.06s-9.72,6.21-9.72,12.65v25.4H49.19V47.41ZM40,31.79a8.42,8.42,0,1,1-8.42-8.42A8.43,8.43,0,0,1,40,31.79ZM23.18,47.41H40V98.94H23.18V47.41Z"/>
+</svg>
+</a>
+
 <a href="https://bsky.app/intent/compose?text=${encodedText}%20${encodedLink}" onclick="window.open(this.href,'_blank','noopener,noreferrer,width=600,height=600');return false;" title="Bagikan ke Bluesky" aria-label="Bagikan ke Bluesky">
 <svg width="20" height="20" viewBox="0 0 511.999 452.266" fill="currentColor">
 <path d="M110.985 30.442c58.695 44.217 121.837 133.856 145.013 181.961 23.176-48.105 86.322-137.744 145.016-181.961 42.361-31.897 110.985-56.584 110.985 21.96 0 15.681-8.962 131.776-14.223 150.628-18.272 65.516-84.873 82.228-144.112 72.116 103.55 17.68 129.889 76.238 73 134.8-108.04 111.223-155.288-27.905-167.385-63.554-3.489-10.262-2.991-10.498-6.561 0-12.098 35.649-59.342 174.777-167.382 63.554-56.89-58.562-30.551-117.12 72.999-134.8-59.239 10.112-125.84-6.6-144.112-72.116C8.962 184.178 0 68.083 0 52.402c0-78.544 68.633-53.857 110.985-21.96z"/>
