981cfe46d02b9af060174de06bac6f7e64dcadff
frijal <5477182+frijal@users.noreply.github.com>
2026-07-19 15:23:17 +0800

    update 07-19

--- dapur/koki.ts
+++ dapur/koki.ts
@@ -4,7 +4,7 @@ import { $ } from "bun";
 
 // --- PENGATURAN KURASI ---
 const menuAndalan = [
-  "dashboard.html", "dashboard.ts", "github.css", "github-dark.css", "github-dark-dimmed.css", "halaman-pencarian.css", "halaman-pencarian.ts", "header.css", "header.ts", "header-logo-atas.html", "homepage.css", "iposbrowser.ts", "json-xml.html", "leaflet.css", "lightbox.ts", "pemandu.css", "pemandu.ts", "response.ts", "sitemap.css", "ujihalaman.html", "homepage.ts", "sitemap.ts", "titleToCategory.ts", "data-provider.ts", "tema-newsprint.ts"
+  "dashboard.html", "dashboard.ts", "github.css", "github-dark.css", "github-dark-dimmed.css", "halaman-pencarian.css", "halaman-pencarian.ts", "header.css", "header.ts", "header-logo-atas.html", "homepage.css", "iposbrowser.ts", "json-xml.html", "leaflet.css", "lightbox.ts", "pemandu.css", "pemandu.ts", "response.ts", "sitemap.css", "ujihalaman.html", "homepage.ts", "sitemap.ts", "data-provider.ts", "tema-newsprint.ts"
 ];
 
 const menuAsli = [
